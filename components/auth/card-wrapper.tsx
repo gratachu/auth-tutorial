feb4e86c9731ff0628a240ae717b19418eaed722
gratachu <akitatsu95.climbing@gmail.com>
2024-01-13 16:26:51 +0900

    Add: card footer

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -7,7 +7,8 @@ import {
   CardHeader
 } from "../ui/card";
 import { Header } from "@/components/auth/header";
-import { Social } from "./social";
+import { Social } from "@/components/auth/social";
+import {BackButton} from "@/components/auth/back-button";
 
 interface CardWrapperProps {
   children: React.ReactNode;
@@ -39,6 +40,14 @@ export const CardWrapper = ({
           <Social />
         </CardFooter>
       )}
+
+      <CardFooter>
+        <BackButton
+          label={backButtonLabel}
+          href={backButtonHref}
+        />
+
+      </CardFooter>
     </Card>
   )
 }
