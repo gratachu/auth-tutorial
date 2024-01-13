a0ecbfb95b2ac126a7a71decbd99d1b6890efa79
gratachu <akitatsu95.climbing@gmail.com>
2024-01-13 21:26:42 +0900

    Add: shadcn form and input

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -43,8 +43,7 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
-    const buttonClass = buttonVariants({ variant, size, className });
-    console.log("buttonClass: ", buttonClass)
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
