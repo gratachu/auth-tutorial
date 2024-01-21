7b8e2bc6947d7411aae9019e489d49bd4ea24d80
gratachu <akitatsu95.climbing@gmail.com>
2024-01-21 20:55:24 +0900

    Add: middleware settings

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,44 @@
-import authConfig from "@/auth.config"
-
 import NextAuth from "next-auth"
 
+import authConfig from "@/auth.config"
+import {
+  DEFAULT_LOGIN_REDIRECT_URL,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes"
+
 const { auth } = NextAuth(authConfig)
+const LOGIN_PATH = "/auth/login"
 
 export default auth((req) => {
+  const  { nextUrl } = req
   const isLoggedIn = !!req.auth
-  console.log("ROUTE: ", req.nextUrl.pathname)
-  console.log("IS LOGGEDIN: ", isLoggedIn)
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+  console.log("nextUrl", nextUrl)
+  console.log("publicRoute", publicRoutes)
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT_URL, nextUrl))
+    }
+
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL(LOGIN_PATH, nextUrl))
+  }
+
+  return null;
 })
 
 export const config = {
