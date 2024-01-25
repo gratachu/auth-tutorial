54aecc8875194dfb4b081a7ac348f2839b1480d4
gratachu <akitatsu95.climbing@gmail.com>
2024-01-25 21:41:24 +0900

    Adding callback session and session.user.role

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
+import {getUserById} from "@/data/user";
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +11,33 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      console.log({ sessionToken: token })
+
+      if ( token.sub && session.user ) {
+        session.user.id = token.sub
+      }
+
+      if ( token.role && session.user ) {
+        session.user.role = token.role
+      }
+
+      return session
+    },
+    async jwt({ token }){
+      if ( !token.sub ) return token
+
+      const existingUser = await getUserById(token.sub)
+
+      if ( !existingUser ) return token
+
+      // @ts-ignore
+      token.role = existingUser.role
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
